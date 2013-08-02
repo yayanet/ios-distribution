72bc5cafabff6a3fae5cf870f49c30711feecd35
yang yu <yuyang239@gmail.com>
2013-08-02 15:39:13 +0800

    Create new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IosDistribution::Application.config.secret_key_base = '9a782990a93b4e7d8a63539340416ba40b2c0242e5680e56dd939e356fa86b7ff49998e7a9e7e2d23205f6f0b7de01a1d0f20b1d4389984932bd40632bf6f05f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IosDistribution::Application.config.session_store :cookie_store, key: '_ios-distribution_session'
